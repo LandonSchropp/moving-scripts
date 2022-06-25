67b07f9a16f463746944491c67e33e3e67cfa2f8
Landon Schropp <schroppl@gmail.com>
2022-06-25 16:26:19 -0500

    Use TypeScript-friendly approach in Notion query

--- source/notion.ts
+++ source/notion.ts
@@ -1,4 +1,18 @@
 import { Client } from "@notionhq/client";
+import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import _ from "lodash";
+import { camelCase } from "voca";
+
+// Extract the Notion types from the exported type in Notion.
+// https://github.com/makenotion/notion-sdk-js/issues/280#issuecomment-1099798305
+type AllKeys<T> = T extends never ? never : keyof T;
+type OptionalKeys<T> = Exclude<AllKeys<T>, keyof T>;
+type Idx<T, K extends PropertyKey, D = never> = T extends never ? never : K extends keyof T ? T[K]
+  : D;
+type Widen<T> = { [K in OptionalKeys<T>]?: Idx<T, K>; } & { [K in keyof T]: T[K] };
+type NotionDatabaseQueryResult = Widen<QueryDatabaseResponse["results"][number]>;
+type NotionPageProperties = NonNullable<NotionDatabaseQueryResult["properties"]>;
+type NotionProperty = NotionPageProperties["type"];
 
 const CITIES_DATABASE_ID = "c0e8bf94ba874800b6e66af872e32ce8";
 
@@ -7,15 +21,29 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN
 });
 
-function notionRowToMetroArea(notionRow) {
-  return {
-    states: notionRow.properties["States"].multi_select.map(value => value.name),
-    population: notionRow.properties["Population"].number,
-    medianHousePrice: notionRow.properties["Median House Price"].number,
-    numberOfSunnyDays: notionRow.properties["Number of Sunny Days"].number,
-    costOfLivingIndex: notionRow.properties["Cost of Living Index"].number,
-    schoolRatingIndex: notionRow.properties["School Rating Index"].number
-  };
+const convertKeyFromNotionFormat = camelCase;
+
+function convertPropertyFromNotionFormat(notionProperty: NotionProperty) {
+  if ("title" in notionProperty) {
+    return notionProperty.title[0].plain_text;
+  }
+
+  if ("number" in notionProperty) {
+    return notionProperty.number;
+  }
+
+  if ("multi_select" in notionProperty) {
+    return notionProperty.multi_select.map(value => value.name);
+  }
+
+  throw new Error(`Unknown type for property ${ JSON.stringify(notionProperty) }`);
+}
+
+function notionRowToMetroArea(notionPage: NotionDatabaseQueryResult) {
+  return _.chain(notionPage.properties)
+    .mapValues(convertPropertyFromNotionFormat)
+    .mapKeys((_value, key) => convertKeyFromNotionFormat(key))
+    .value();
 }
 
 export async function fetchCities() {

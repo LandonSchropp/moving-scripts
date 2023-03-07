e5839df7dbc7c8b9022c7ecf3196e01ae96affb1
Landon Schropp <schroppl@gmail.com>
2023-03-07 00:07:05 -0800

    fixup! Genericize Notion logic

--- source/notion-metro-areas.ts
+++ source/notion-metro-areas.ts
@@ -4,30 +4,30 @@ import { ExtendedMetroArea } from "./types";
 const CITIES_DATABASE_ID = "c0e8bf94ba874800b6e66af872e32ce8";
 
 const SCHEMA = {
-  "cities": "title",
-  "states": "multi_select",
-  "population": "number",
+  cities: "title",
+  states: "multi_select",
+  population: "number",
 
   // Housing
-  "topTierHousingPrice": "number",
-  "middleTierHousingPrice": "number",
-  "bottomTierHousingPrice": "number",
-  "threeBedroomHousingPrice": "number",
+  topTierHousingPrice: "number",
+  middleTierHousingPrice: "number",
+  bottomTierHousingPrice: "number",
+  threeBedroomHousingPrice: "number",
 
   // Politics
-  "winnerOf2020Election": "select",
-  "winnerOf2020ElectionVotePercentage": "number",
+  winnerOf2020Election: "select",
+  winnerOf2020ElectionVotePercentage: "number",
 
   // Weather
-  "summerHighTemperature": "number",
-  "winterLowTemperature": "number",
-  "rainfall": "number",
-  "snowfall": "number",
-  "numberOfDaysWithPrecipitation": "number",
-  "numberOfSunnyDays": "number",
-  "comfortIndex": "number",
-  "summerComfortIndex": "number",
-  "winterComfortIndex": "number"
+  summerHighTemperature: "number",
+  winterLowTemperature: "number",
+  rainfall: "number",
+  snowfall: "number",
+  numberOfDaysWithPrecipitation: "number",
+  numberOfSunnyDays: "number",
+  comfortIndex: "number",
+  summerComfortIndex: "number",
+  winterComfortIndex: "number"
 } as const;
 
 export async function syncMetroAreasToNotion(metroAreas: ExtendedMetroArea[]) {
